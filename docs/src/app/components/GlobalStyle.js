edfd19419bffc7f55bb38b8245064e12d00395c6
Mahdi Afzal <mahdiafzal@gmail.com>
2018-12-03 17:56:16 +0330

    added folders

---
+++ docs/src/app/components/GlobalStyle.js
@@ -0,0 +1,35 @@
+
+// import injectGlobal helper
+import { createGlobalStyle } from 'styled-components'
+
+// Global style
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  #app,
+  .wrapper {
+    min-height: 100vh;
+    height: 100%;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 100%;
+  }
+
+  * {
+    &,
+    &::after,
+    &::before {
+      box-sizing: inherit;
+    }
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font: 1rem / 1.414 arial, sans-serif;
+  }
+`;
+
+export default GlobalStyle
